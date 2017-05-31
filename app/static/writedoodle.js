744a808329ee41365bcc180b1c32687014fcaa3b
Michael Hirsch <michael.hirsch@rga.com>
2017-05-31 16:59:09 -0400

    more

--- app/static/writedoodle.js
+++ app/static/writedoodle.js
@@ -2,8 +2,6 @@
  * Created by michael hirsch on 5/25/17.
  */
 
-console.log("michael hirsch");
-
 var c = document.getElementById('drawing');
 var ctx = c.getContext('2d');
 
@@ -14,60 +12,66 @@ var words = [];
 var doodle_count = -1;
 var currentWord = "";
 
-function draw_doodle(vertices){
-
+function draw_doodle(draw_verts, ind){
+    var vertices = Object.values(draw_verts[ind])[0];
+    var at = parseInt(Object.keys(draw_verts[0])[0]);
+    console.log(at);
     ctx.lineWidth = 3;
     ctx.beginPath();
-    ctx.moveTo(vertices[0].x+doodle_count*256, vertices[0].y);
+    ctx.moveTo(vertices[0].x+at*256, vertices[0].y);
     var i = 0;
     var interval = setInterval(function(){
         var pt = vertices[i];
-
         var prevPt = vertices[i > 0 ? i - 1 : 0];
-        ctx.moveTo(prevPt.x+doodle_count*256, prevPt.y)
-        ctx.lineTo(pt.x+doodle_count*256, pt.y);
+        ctx.moveTo(prevPt.x+at*256, prevPt.y)
+        ctx.lineTo(pt.x+at*256, pt.y);
         ctx.stroke();
-        i ++;
+        i++;
         if(i >= vertices.length){
+            ind+=1;
+            if(ind < draw_verts.length){
+                draw_doodle(draw_verts, ind);
+            }
             clearInterval(interval);
         }
-    }, 40);
+    }, 30);
 }
 
-function findDoodle(currentWord){
+function findDoodle(currentWord, doodle_num){
     $.get('/api/find_doodle/'+currentWord, function(response){
-    doodle_count+=1;
 
-        console.log(response)
+        var all_verts = [];
         response.image.forEach(function(el){
-            vertices = [];
+            var vertices = [];
             for (var i=0; i < el[0].length; i++){
                 vertices.push({
                         x: el[0][i],
                         y: el[1][i]
                     })
                 }
-            draw_doodle(vertices);
+            console.log("this is doodle_num " + doodle_num);
+            var o = {};
+            o[doodle_num] = vertices;
+            all_verts.push(o);
         });
+        draw_doodle(all_verts, 0);
     })
 }
 
 
 $('body').on('keydown', function(t){
-    console.log(t.keyCode);
 
     if ((t.keyCode>=65 && t.keyCode<=90) || t.keyCode ===32){
         ctx.fillText(t.key,10*i,20);
         i+=1;
-        console.log(i);
         prev=t.key;
         currentWord+=t.key;
     }
 
     if (t.keyCode===13){
-        console.log("this is the word: " + currentWord);
         words.push(currentWord);
-        findDoodle(currentWord);
+        doodle_count+=1;
+        findDoodle(currentWord, doodle_count);
         currentWord = "";
     }
 
